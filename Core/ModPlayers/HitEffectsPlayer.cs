514f42b2da9eb4f6b717c9907d1dfc81c01ca647
JavyzTaken <jacklukas@outlook.com>
2024-11-03 15:00:18 +0100

    Spider Enchant crits are +50% damage if held weapon is of non-Summoner class

--- Core/ModPlayers/HitEffectsPlayer.cs
+++ Core/ModPlayers/HitEffectsPlayer.cs
@@ -90,6 +90,8 @@ namespace FargowiltasSouls.Core.ModPlayers
                             SoundEngine.PlaySound(SoundID.Item147 with { Pitch = 1, Volume = 0.7f }, target.Center);
                         }
                     }
+                    if (MinionCrits && damageClass.CountsAsClass(DamageClass.Summon) && !Player.ProcessDamageTypeFromHeldItem().CountsAsClass(DamageClass.Summon))
+                        hitInfo.Damage = (int)(hitInfo.Damage * 0.75);
                 }
 
                 if (Hexed)
