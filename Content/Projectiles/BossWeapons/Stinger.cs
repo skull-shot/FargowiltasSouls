7a67fe3d7eb1ccc203527c7c90e8fa11f5c0bba1
schmoovi <romandark1@gmail.com>
2025-01-03 15:51:37 -0600

    iframe fixes

--- Content/Projectiles/BossWeapons/Stinger.cs
+++ Content/Projectiles/BossWeapons/Stinger.cs
@@ -24,8 +24,8 @@ namespace FargowiltasSouls.Content.Projectiles.BossWeapons
             Projectile.friendly = true;
             Projectile.DamageType = DamageClass.Ranged;
             Projectile.timeLeft = 240;
-            Projectile.usesLocalNPCImmunity = true;
-            Projectile.localNPCHitCooldown = 10;
+            Projectile.usesIDStaticNPCImmunity = true;
+            Projectile.idStaticNPCHitCooldown = 10;
         }
 
         public override void AI()
