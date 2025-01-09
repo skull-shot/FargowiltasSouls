0f1130797d50597f890f83ed1bf766b563f4a309
JavyzTaken <jacklukas@outlook.com>
2025-01-09 21:08:13 +0100

    -Tin crit doesn't reset when damage taken is less than 5 Terra Force: -Tin crit now begins at 50% and goes up by 25% for each lightning bolt that's fired -Parrying now sends 2 lightning bolts -Lightning bolts are more frequent at higher Tin crit chance

--- Core/ModPlayers/AccessoryEffectsPlayer.cs
+++ Core/ModPlayers/AccessoryEffectsPlayer.cs
@@ -843,7 +843,8 @@ namespace FargowiltasSouls.Core.ModPlayers
                             int targetID = FargoSoulsUtil.FindClosestHostileNPC(Player.Center, 1000, true, true);
                             if (targetID.IsWithinBounds(Main.maxNPCs) && Main.npc[targetID] is NPC target && target.Alive())
                             {
-                                TerraLightningEffect.LightningProc(Player, target, 4f);
+                                for (int i = 0; i < 2; i++)
+                                    TerraLightningEffect.LightningProc(Player, target, 4f);
                             }
                             
                         }
