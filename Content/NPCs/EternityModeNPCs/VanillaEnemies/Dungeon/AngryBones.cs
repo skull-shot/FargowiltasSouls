b9a90ac4f081bbb22c07fa1d5cf266ff43fc1c0a
Agla <boyblob@gmail.com>
2025-03-12 12:53:36 -0700

    mr. spear

--- Content/NPCs/EternityModeNPCs/VanillaEnemies/Dungeon/AngryBones.cs
+++ Content/NPCs/EternityModeNPCs/VanillaEnemies/Dungeon/AngryBones.cs
@@ -4,6 +4,7 @@ using FargowiltasSouls.Core.NPCMatching;
 using Microsoft.Xna.Framework;
 using System;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -20,6 +21,14 @@ namespace FargowiltasSouls.Content.NPCs.EternityModeNPCs.VanillaEnemies.Dungeon
 
         //public int BoneSprayTimer;
         public int BabyTimer;
+        public override void OnSpawn(NPC npc, IEntitySource source)
+        {
+            if (FargoSoulsUtil.HostCheck)
+            {
+                Projectile.NewProjectileDirect(source, npc.Center, Vector2.Zero, ModContent.ProjectileType<BoneSpear>(), FargoSoulsUtil.ScaledProjectileDamage(npc.damage), 1, -1, npc.whoAmI);
+            }
+            base.OnSpawn(npc, source);
+        }
 
         public override void AI(NPC npc)
         {
@@ -36,41 +45,41 @@ namespace FargowiltasSouls.Content.NPCs.EternityModeNPCs.VanillaEnemies.Dungeon
             //        Projectile.NewProjectile(npc.GetSource_FromThis(), npc.Center, speed, ProjectileID.SkeletonBone, FargoSoulsUtil.ScaledProjectileDamage(NPC.defDamage), 0f, Main.myPlayer);
             //}
 
-            if (npc.justHit)
-            {
-                //BoneSprayTimer = 120;
-                BabyTimer += 20;
-            }
+            //if (npc.justHit)
+            //{
+            //    //BoneSprayTimer = 120;
+            //    BabyTimer += 20;
+            //}
 
-            if (++BabyTimer > 300) //shoot baby guardians
-            {
-                BabyTimer = 0;
-                if (FargoSoulsUtil.HostCheck && npc.HasValidTarget && Collision.CanHitLine(npc.Center, 0, 0, Main.player[npc.target].Center, 0, 0))
-                    Projectile.NewProjectile(npc.GetSource_FromThis(), npc.Center, npc.SafeDirectionTo(Main.player[npc.target].Center), ModContent.ProjectileType<SkeletronGuardian2>(), FargoSoulsUtil.ScaledProjectileDamage(npc.defDamage), 0f, Main.myPlayer);
-            }
+            //if (++BabyTimer > 300) //shoot baby guardians
+            //{
+            //    BabyTimer = 0;
+            //    if (FargoSoulsUtil.HostCheck && npc.HasValidTarget && Collision.CanHitLine(npc.Center, 0, 0, Main.player[npc.target].Center, 0, 0))
+            //        Projectile.NewProjectile(npc.GetSource_FromThis(), npc.Center, npc.SafeDirectionTo(Main.player[npc.target].Center), ModContent.ProjectileType<SkeletronGuardian2>(), FargoSoulsUtil.ScaledProjectileDamage(npc.defDamage), 0f, Main.myPlayer);
+            //}
         }
 
         public override void OnKill(NPC npc)
         {
             base.OnKill(npc);
 
-            if (FargoSoulsUtil.HostCheck)
-            {
-                if (Main.rand.NextBool(5))
-                    FargoSoulsUtil.NewNPCEasy(npc.GetSource_FromAI(), npc.Center, NPCID.CursedSkull);
+            //if (FargoSoulsUtil.HostCheck)
+            //{
+            //    if (Main.rand.NextBool(5))
+            //        FargoSoulsUtil.NewNPCEasy(npc.GetSource_FromAI(), npc.Center, NPCID.CursedSkull);
 
-                for (int i = 0; i < 15; i++)
-                {
-                    Vector2 speed = new(Main.rand.Next(-50, 51), Main.rand.Next(-100, 1));
-                    speed.Normalize();
-                    speed *= Main.rand.NextFloat(3f, 6f);
-                    speed.Y -= Math.Abs(speed.X) * 0.2f;
-                    speed.Y -= 3f;
-                    speed.Y *= Main.rand.NextFloat(1.5f);
-                    if (FargoSoulsUtil.HostCheck)
-                        Projectile.NewProjectile(npc.GetSource_FromThis(), npc.Center, speed, ProjectileID.SkeletonBone, FargoSoulsUtil.ScaledProjectileDamage(npc.defDamage), 0f, Main.myPlayer);
-                }
-            }
+            //    for (int i = 0; i < 15; i++)
+            //    {
+            //        Vector2 speed = new(Main.rand.Next(-50, 51), Main.rand.Next(-100, 1));
+            //        speed.Normalize();
+            //        speed *= Main.rand.NextFloat(3f, 6f);
+            //        speed.Y -= Math.Abs(speed.X) * 0.2f;
+            //        speed.Y -= 3f;
+            //        speed.Y *= Main.rand.NextFloat(1.5f);
+            //        if (FargoSoulsUtil.HostCheck)
+            //            Projectile.NewProjectile(npc.GetSource_FromThis(), npc.Center, speed, ProjectileID.SkeletonBone, FargoSoulsUtil.ScaledProjectileDamage(npc.defDamage), 0f, Main.myPlayer);
+            //    }
+            //}
         }
     }
 }
---
+++ Content/Projectiles/Masomode/BoneSpear.cs
@@ -0,0 +1,134 @@
+﻿using FargowiltasSouls.Content.NPCs.EternityModeNPCs.VanillaEnemies.Dungeon;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Content.Projectiles.Masomode
+{
+    public class BoneSpear : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = Projectile.height = 6;
+            Projectile.tileCollide = false;
+            Projectile.hostile = true;
+            Projectile.friendly = false;
+            Projectile.FargoSouls().DeletionImmuneRank = 1;
+            Projectile.aiStyle = -1;
+            Projectile.hide = true;
+            Projectile.penetrate = -1;
+            base.SetDefaults();
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Asset<Texture2D> t = TextureAssets.Projectile[Type];
+            Asset<Texture2D> head = ModContent.Request<Texture2D>("FargowiltasSouls/Content/Projectiles/Masomode/BoneSpearHead");
+            Vector2 origin = t.Size() - new Vector2(16, 16);
+            lightColor = Lighting.GetColor(Projectile.Center.ToTileCoordinates());
+            if (Projectile.ai[0] < 0)
+            {
+                origin = t.Size() / 2;
+            }
+            
+            for (int j = 0; j < 10; j++)
+            {
+                Vector2 afterimageOffset = (MathHelper.TwoPi * j / 12f).ToRotationVector2() * 1 ;
+                Color glowColor = Color.Blue with { A = 0 } * 0.7f;
+
+
+                Main.EntitySpriteDraw(head.Value, Projectile.Center + afterimageOffset - Main.screenPosition, null, glowColor, Projectile.rotation, origin, Projectile.scale, SpriteEffects.None);
+            }
+            Main.EntitySpriteDraw(t.Value, Projectile.Center - Main.screenPosition, null, lightColor, Projectile.rotation, origin, 1, SpriteEffects.None);
+            return false;
+        }
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        {
+            behindNPCs.Add(index);
+            base.DrawBehind(index, behindNPCsAndTiles, behindNPCs, behindProjectiles, overPlayers, overWiresUI);
+        }
+        public override void OnKill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.Dig, Projectile.Center);
+            base.OnKill(timeLeft);
+        }
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            Vector2 vec = Projectile.Center - new Vector2(29, 29).RotatedBy(Projectile.rotation) * Projectile.scale;
+            if (Projectile.ai[0] < 0)
+            {
+                vec = Projectile.Center - new Vector2(19, 19).RotatedBy(Projectile.rotation) * Projectile.scale;
+            }
+            Point center = vec.ToPoint();
+            
+            
+            Rectangle hitbox = new Rectangle((int)vec.X - 3, (int)vec.Y - 3, 6, 6);
+            return targetHitbox.Intersects(hitbox);
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            if (Collision.SolidCollision(Projectile.TopLeft, Projectile.width, -6))
+            {
+                Projectile.velocity.Y = 5;
+                
+                Projectile.velocity.X = oldVelocity.X;
+                return false;
+            }
+            if (Projectile.oldVelocity.Y <= 0)
+            {
+                return false;
+            }
+            
+            return base.OnTileCollide(oldVelocity);
+        }
+        public static int[] Bones = [NPCID.AngryBones,
+            NPCID.AngryBonesBig,
+            NPCID.AngryBonesBigHelmet,
+            NPCID.AngryBonesBigMuscle];
+        public override void AI()
+        {
+            
+            if ((int)Projectile.ai[0] >= 0 && (Main.npc[(int)Projectile.ai[0]] == null || !Main.npc[(int)Projectile.ai[0]].active || !Bones.Contains(Main.npc[(int)Projectile.ai[0]].type)))
+            {
+                
+                Projectile.ai[0] = -1;
+            }
+            if (Projectile.ai[0] == -1)
+            {
+                Projectile.tileCollide = true;
+                Projectile.velocity = new Vector2(0, -8).RotatedByRandom(MathHelper.ToRadians(10));
+                Projectile.ai[0] = -2;
+            }else if (Projectile.ai[0] == -2)
+            {
+                
+                Projectile.velocity += new Vector2(0, 0.2f);
+                Projectile.rotation += MathHelper.ToRadians(Projectile.velocity.Length() * 2);
+            }
+            else
+            {
+                Projectile.timeLeft = 200;
+                NPC owner = Main.npc[(int)Projectile.ai[0]];
+                Projectile.Center = owner.Center;
+                Projectile.velocity = Vector2.Zero;
+                if (owner.spriteDirection == 1)
+                {
+                    Projectile.rotation = MathHelper.ToRadians(135);
+                }
+                else
+                {
+                    Projectile.rotation = MathHelper.ToRadians(315);
+                }
+            }
+            base.AI();
+        }
+    }
+}
