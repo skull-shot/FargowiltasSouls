905f8671f8379da74b95ad07dc2da68f783a21ea
JavyzTaken <jacklukas@outlook.com>
2025-03-29 13:06:21 +0100

    pumpkin ench 20 -> 18, fossil 15 -> 12

--- Content/Projectiles/Souls/GrowingPumpkin.cs
+++ Content/Projectiles/Souls/GrowingPumpkin.cs
@@ -84,7 +84,7 @@ namespace FargowiltasSouls.Content.Projectiles.Souls
                     {
                         if (Main.myPlayer == hitPlayer.whoAmI)
                         {
-                            int heal = 20;
+                            int heal = 18;
                             if (modPlayer.ForceEffect<PumpkinEnchant>())
                                 heal *= 2;
                             FargoGlobalItem.OnRetrievePickup(hitPlayer);
