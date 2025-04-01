86b428d9b2c637623e586837f4bd02dba08664d5
Habble53 <87115317+Habble53@users.noreply.github.com>
2025-04-01 19:10:51 +0300

    2.0 evils minor balance
    Eyeleash base damage 14 -> 9, servants base damage 1.6x -> 2x, spawn rate 2/s -> 3/s
    Sisyphean Fist base damage 82 -> 70, initial throw base projectile velocity 20f -> 22f

--- Content/Items/Weapons/BossDrops/LeashOfCthulhu.cs
+++ Content/Items/Weapons/BossDrops/LeashOfCthulhu.cs
@@ -15,7 +15,7 @@ namespace FargowiltasSouls.Content.Items.Weapons.BossDrops
 
         public override void SetDefaults()
         {
-            Item.damage = 14;
+            Item.damage = 9;
             Item.width = 30;
             Item.height = 10;
             Item.value = Item.sellPrice(0, 1);
