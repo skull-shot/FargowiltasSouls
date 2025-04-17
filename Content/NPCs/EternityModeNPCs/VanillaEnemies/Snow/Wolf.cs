e988f50d5f3a0d308ff5038e97785b6933bb3b03
Beetle <jiji>
2025-04-17 18:19:34 -0400

    wolf rework

--- Content/NPCs/EternityModeNPCs/VanillaEnemies/Snow/Wolf.cs
+++ Content/NPCs/EternityModeNPCs/VanillaEnemies/Snow/Wolf.cs
@@ -1,9 +1,14 @@
-﻿using FargowiltasSouls.Core.Globals;
+﻿using FargowiltasSouls.Assets.Sounds;
+using FargowiltasSouls.Content.Projectiles.Masomode.Enemies.Vanilla.Snow;
+using FargowiltasSouls.Core.Globals;
 using FargowiltasSouls.Core.NPCMatching;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System.IO;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ID;
+using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 
 namespace FargowiltasSouls.Content.NPCs.EternityModeNPCs.VanillaEnemies.Snow
@@ -30,7 +35,7 @@ namespace FargowiltasSouls.Content.NPCs.EternityModeNPCs.VanillaEnemies.Snow
         public override void OnFirstTick(NPC npc)
         {
             base.OnFirstTick(npc);
-
+            return;
             if (Main.rand.NextBool(3) && npc.FargoSouls().CanHordeSplit)
                 EModeGlobalNPC.Horde(npc, Main.rand.Next(10) + 1);
         }
@@ -38,24 +43,39 @@ namespace FargowiltasSouls.Content.NPCs.EternityModeNPCs.VanillaEnemies.Snow
         public override bool SafePreAI(NPC npc)
         {
             AttackTimer++;
+            int timeToCharge = 220;
             if (AttackTimer >= 180)
             {
                 if (AttackTimer == 180)
                 {
-
+                    FargoSoulsUtil.DustRing(npc.Center, 30, DustID.FrostStaff, 7f);
+                    SoundEngine.PlaySound(SoundID.NPCHit6 with { Pitch = -0.9f }, npc.Center);
                 }
-                if (AttackTimer == 200 && npc.HasValidTarget)
+                else if (AttackTimer < timeToCharge)
                 {
-                    targetPos = Main.player[npc.target].position;
-                    Vector2 dist = targetPos - npc.position;
-                    npc.velocity = 10 * Vector2.UnitX.RotatedBy(dist.ToRotation());
+                    npc.velocity *= 0.98f;
                 }
-                if (AttackTimer >= 200)
+                else if (AttackTimer == timeToCharge && npc.HasValidTarget && Collision.CanHitLine(npc.Center, 1, 1, Main.player[npc.target].Center, 1, 1))
                 {
-                    npc.velocity.Y += 0.1f;
-                    if (Collision.SolidCollision(npc.position, npc.width, npc.height))
+                    Vector2 targetPoint = Main.player[npc.target].Center - Vector2.UnitY * 200;
+                    float distanceScale = MathHelper.Clamp(npc.Distance(targetPoint) / 1000f, 0f, 1f);
+                    float vel = 5f + 20f * distanceScale;
+                    npc.velocity = npc.DirectionTo(targetPoint) * vel;
+                    SoundEngine.PlaySound(FargosSoundRegistry.ThrowShort with { Pitch = 0.5f }, npc.Center);
+                }
+                else if (AttackTimer >= timeToCharge)
+                {
+                    if (npc.velocity.Y == 0)
                     {
                         AttackTimer = 0;
+                    }
+                    else
+                    {
+                        if (AttackTimer % 4 == 0 && FargoSoulsUtil.HostCheck)
+                        {
+                            Projectile.NewProjectile(npc.GetSource_FromThis(), npc.Center, Vector2.Zero, ModContent.ProjectileType<SnowMist>(), npc.damage / 8, 0f);
+                        }
+                        npc.velocity.Y += 0.01f;
                     }
                 }
                 return false;
@@ -70,6 +90,11 @@ namespace FargowiltasSouls.Content.NPCs.EternityModeNPCs.VanillaEnemies.Snow
 
             target.AddBuff(BuffID.Bleeding, 300);
             target.AddBuff(BuffID.Rabies, 900);
+        }
+
+        public override bool PreDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            return base.PreDraw(npc, spriteBatch, screenPos, drawColor);
         }
     }
 }
