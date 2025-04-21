eb0efe5e4e43275e9ee595eae85cf1304752a22d
Midnight295 <143055111+Midnight295@users.noreply.github.com>
2025-04-20 17:48:51 -0700

    Added emotes for all of our bosses. Added an Enchantment Emote, and a Mutant's Gift emote.

---
+++ Content/UI/Emotes/WillEmote.cs
@@ -0,0 +1,19 @@
+﻿using FargowiltasSouls.Core.Systems;
+using Terraria.GameContent.UI;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Content.UI.Emotes
+{
+    public class WillEmote : ModEmoteBubble
+    {
+        public override void SetStaticDefaults()
+        {
+            AddToCategory(EmoteID.Category.Dangers);
+        }
+
+        public override bool IsUnlocked()
+        {
+            return WorldSavingSystem.DownedBoss[7];
+        }
+    }
+}
