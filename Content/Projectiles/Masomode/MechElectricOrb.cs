fe21c622dfb2e68ef370025c1f72ebca5c80f1e3
schmoovi <romandark1@gmail.com>
2025-06-18 02:40:57 -0500

    Quieted Electric Orb projectile sound

--- Content/Projectiles/Masomode/MechElectricOrb.cs
+++ Content/Projectiles/Masomode/MechElectricOrb.cs
@@ -77,7 +77,7 @@ namespace FargowiltasSouls.Content.Projectiles.Masomode
             }
             if (Projectile.localAI[1] == 0)
             {
-                SoundEngine.PlaySound(ShotSound with { Volume = 0.5f, MaxInstances = 4 }, Projectile.position);
+                SoundEngine.PlaySound(ShotSound with { Volume = 0.3f, MaxInstances = 4 }, Projectile.position);
                 Projectile.localAI[1] = 1f;
 
                 SoundEngine.PlaySound(HumSound with { 
