d6bb24f7372a00c07e2df5569e3f24314ea51e04
JavyzTaken <jacklukas@outlook.com>
2025-07-11 18:34:18 +0200

    Champion of Will: -Added golden flame trails to dashes instead of dust (derived from emode calclone) -Added glow outlines to javelins -Added gold dust geyser to telegraph fireball spit (less telegraph time on Maso) -Slightly increased telegraph time of the first volley of each undyne javelin attack -Decreased width of deathrays, except on Masochist
    Masochist Mode buffs:
    -Phase transition undyne javelin rings rotate in a random direction
    -Spawns a triplet of undyne javelins each dash (except during typhoon circle attack)

--- Content/Bosses/Champions/Will/WillJavelin.cs
+++ Content/Bosses/Champions/Will/WillJavelin.cs
@@ -96,6 +96,16 @@ namespace FargowiltasSouls.Content.Bosses.Champions.Will
                 Main.EntitySpriteDraw(texture2D13, value4 + Projectile.Size / 2f - Main.screenPosition + new Vector2(0, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color27, num165, origin2, Projectile.scale, effects, 0);
             }
 
+            Main.spriteBatch.UseBlendState(BlendState.Additive);
+            for (int j = 0; j < 12; j++)
+            {
+                Vector2 afterimageOffset = (MathHelper.TwoPi * j / 12).ToRotationVector2() * 2f * Projectile.scale;
+                Color glowColor = Color.Goldenrod;
+
+                Main.EntitySpriteDraw(texture2D13, Projectile.Center + afterimageOffset - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), rectangle, glowColor, Projectile.rotation, origin2, Projectile.scale, effects);
+            }
+            Main.spriteBatch.ResetToDefault();
+
             Main.EntitySpriteDraw(texture2D13, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), Projectile.GetAlpha(lightColor), Projectile.rotation, origin2, Projectile.scale, effects, 0);
             return false;
         }
