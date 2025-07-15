f634f0aab2637b607066f98086019786441d1050
JavyzTaken <jacklukas@outlook.com>
2025-07-15 17:09:21 +0200

    grab dps 12 -> 10

--- Core/ModPlayers/UpdatesPlayer.cs
+++ Core/ModPlayers/UpdatesPlayer.cs
@@ -434,7 +434,7 @@ namespace FargowiltasSouls.Core.ModPlayers
                 Player.lifeRegen -= 16;
 
             if (GrabDamage)
-                Player.lifeRegen -= 24;
+                Player.lifeRegen -= 20;
 
             void DamageOverTime(int badLifeRegen, bool affectLifeRegenCount = false)
             {
