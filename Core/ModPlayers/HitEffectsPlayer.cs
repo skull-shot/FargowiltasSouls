6b1cbbb8bc3b8f785b89b9e74b0cac85eb9b2ff0
schmoovi <romandark1@gmail.com>
2025-07-26 02:34:32 -0500

    Fixed misleading nerf tooltip on Gray/Orange Zapinator

--- Core/ModPlayers/HitEffectsPlayer.cs
+++ Core/ModPlayers/HitEffectsPlayer.cs
@@ -525,7 +525,7 @@ namespace FargowiltasSouls.Core.ModPlayers
                 Player.AddBuff(ModContent.BuffType<TitaniumCDBuff>(), LumUtils.SecondsToFrames(15));
             }
 
-            if (DeerSinewNerf && DeerSinewFreezeCD <= 0 && (info.DamageSource.SourceNPCIndex.IsWithinBounds(Main.maxNPCs) || (info.DamageSource.SourceProjectileType.IsWithinBounds(Main.maxProjectiles) && Main.projectile[info.DamageSource.SourceProjectileType].aiStyle != ProjAIStyleID.FallingTile)))
+            if (DeerSinewNerf && DeerSinewFreezeCD <= 0 && info.Damage <= 10 && (info.DamageSource.SourceNPCIndex.IsWithinBounds(Main.maxNPCs) || (info.DamageSource.SourceProjectileType.IsWithinBounds(Main.maxProjectiles) && Main.projectile[info.DamageSource.SourceProjectileType].aiStyle != ProjAIStyleID.FallingTile)))
             {
                 DeerSinewFreezeCD = 120;
                 FargoSoulsUtil.AddDebuffFixedDuration(Player, BuffID.Frozen, 20);
