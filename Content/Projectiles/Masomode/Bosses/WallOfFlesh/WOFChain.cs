31c1fe05776f40a7def274e14513d4cb1879fd51
JavyzTaken <jacklukas@outlook.com>
2025-07-29 13:16:51 +0200

    Eternity+ Wall of Flesh: Remade telegraph for purple chain barrages

--- Content/Projectiles/Masomode/Bosses/WallOfFlesh/WOFChain.cs
+++ Content/Projectiles/Masomode/Bosses/WallOfFlesh/WOFChain.cs
@@ -1,9 +1,12 @@
 using FargowiltasSouls.Content.Buffs.Boss;
+using FargowiltasSouls.Content.Projectiles.Masomode.Bosses.MechanicalBosses;
 using FargowiltasSouls.Core.Globals;
 using FargowiltasSouls.Core.Systems;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Terraria;
 using Terraria.GameContent;
@@ -15,6 +18,12 @@ namespace FargowiltasSouls.Content.Projectiles.Masomode
     public class WOFChain : ModProjectile
     {
         public int BittenPlayer = -1;
+        Vector2 OriginPosition = Vector2.Zero;
+        public Vector2 LockPosition => new(Projectile.ai[0], Projectile.ai[1]);
+        public float Angle => Projectile.ai[2];
+        public int Timer = 0;
+        public bool Telegraphing => Timer < TelegraphTime;
+        public int TelegraphTime => 130 * Projectile.MaxUpdates;
         public override string Texture => "Terraria/Images/NPC_115";
 
         public override void SetStaticDefaults()
@@ -38,22 +47,23 @@ namespace FargowiltasSouls.Content.Projectiles.Masomode
             //CooldownSlot = 1;
 
             Projectile.extraUpdates = 2;
+            Projectile.hide = true;
         }
 
-        public override bool? CanDamage() => (Projectile.timeLeft <= 30 || Projectile.ai[2] == 1 || BittenPlayer != -1) ? false : base.CanDamage();
+        public override bool? CanDamage() => Telegraphing || (Projectile.timeLeft <= 30 || Projectile.localAI[2] == 1 || BittenPlayer != -1) ? false : base.CanDamage();
         public override void SendExtraAI(BinaryWriter writer)
         {
             writer.Write7BitEncodedInt(BittenPlayer);
         }
         public override void ReceiveExtraAI(BinaryReader reader)
         {
-                BittenPlayer = reader.Read7BitEncodedInt();
+            BittenPlayer = reader.Read7BitEncodedInt();
         }
         public override void AI()
         {
             Lighting.AddLight(Projectile.Center, 0.1f, 0.5f, 0.7f);
 
-            if (Projectile.timeLeft <= 30 || Projectile.ai[2] == 1)
+            if (Projectile.timeLeft <= 30 || Projectile.localAI[2] == 1)
             {
                 Projectile.Opacity = MathHelper.Lerp(Projectile.Opacity, 0f, 0.05f);
                 if (Projectile.Opacity < 0.1f)
@@ -63,64 +73,81 @@ namespace FargowiltasSouls.Content.Projectiles.Masomode
                 }
             }
 
-            if (Projectile.ai[0] == 0)
+            if (Projectile.localAI[0] == 0)
             {
-                Projectile.ai[0] = 1;
-                Projectile.localAI[0] = Projectile.Center.X;
-                Projectile.localAI[1] = Projectile.Center.Y;
+                Projectile.localAI[0] = 1;
+                OriginPosition = Projectile.Center;
                 //Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value = Main.npcTexture[NPCID.TheHungry];
             }
 
-            if (Projectile.velocity != Vector2.Zero && Main.rand.NextBool(3))
+            Timer++;
+            if (Timer < TelegraphTime) // telegraphing
             {
-                int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GemSapphire, Projectile.velocity.X * 0.4f, Projectile.velocity.Y * 0.4f, 114, default, 2f);
-                Main.dust[dust].noGravity = true;
-                Main.dust[dust].velocity *= 1.8f;
-                Main.dust[dust].velocity.Y -= 0.5f;
+                Projectile.rotation = Projectile.DirectionTo(LockPosition).ToRotation();
+                Vector2 pos = LockPosition - Angle.ToRotationVector2() * 370;
+                pos += Main.rand.NextVector2Circular(5, 5);
+                Projectile.velocity = FargoSoulsUtil.SmartAccel(Projectile.Center, pos, Projectile.velocity, 0.5f, 0.07f);
             }
-
-            //stop moving at vertical limits of underworld
-            if (Projectile.velocity.Y > 0 && Projectile.Center.Y / 16 >= Main.maxTilesY
-                || Projectile.velocity.Y < 0 && Projectile.Center.Y / 16 <= Main.maxTilesY - 200)
+            else if (Timer == TelegraphTime) // telegraph exactly
             {
-                Projectile.position -= Projectile.velocity * 2f;
-                Projectile.velocity = Vector2.Zero;
+                Projectile.velocity = -Angle.ToRotationVector2() * 8;
             }
-
-            if (BittenPlayer != -1)
+            else
             {
-
-                Player victim = Main.player[BittenPlayer];
-                if (victim.active && !victim.ghost && !victim.dead
-                    && (Projectile.Distance(victim.Center) < 160 || victim.whoAmI != Main.myPlayer)
-                    && victim.FargoSouls().MashCounter < 20)
+                if (Projectile.velocity != Vector2.Zero && Main.rand.NextBool(3))
                 {
-                    victim.AddBuff(ModContent.BuffType<GrabbedBuff>(), 2);
-                    victim.velocity = Vector2.Zero;
-                    Projectile.Center = victim.Center;
+                    int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GemSapphire, Projectile.velocity.X * 0.4f, Projectile.velocity.Y * 0.4f, 114, default, 2f);
+                    Main.dust[dust].noGravity = true;
+                    Main.dust[dust].velocity *= 1.8f;
+                    Main.dust[dust].velocity.Y -= 0.5f;
                 }
-                else
+
+                if (Projectile.velocity.LengthSquared() < 15 * 15) // accelerate
+                    Projectile.velocity += Angle.ToRotationVector2() * 0.15f;
+
+                //stop moving at vertical limits of underworld
+                if (Projectile.velocity.Y > 0 && Projectile.Center.Y / 16 >= Main.maxTilesY
+                    || Projectile.velocity.Y < 0 && Projectile.Center.Y / 16 <= Main.maxTilesY - 450)
                 {
-                    BittenPlayer = -1;
-                    Projectile.netUpdate = true;
+                    Projectile.position -= Projectile.velocity * 2f;
+                    Projectile.velocity = Vector2.Zero;
                 }
-            }
 
-            if (FargoSoulsUtil.BossIsAlive(ref EModeGlobalNPC.wallBoss, NPCID.WallofFlesh)
-                && Math.Abs(Projectile.Center.X - Main.npc[EModeGlobalNPC.wallBoss].Center.X) < 50)
-            {
-                Projectile.ai[2] = 1; //chain dies when wall moves over it
-            }
-
-            if (Projectile.velocity != Vector2.Zero)
-            {
-                Projectile.rotation = Projectile.velocity.ToRotation();
-
-                if (++Projectile.frameCounter > 6 * (Projectile.extraUpdates + 1))
+                if (BittenPlayer != -1)
                 {
-                    Projectile.frameCounter = 0;
-                    if (++Projectile.frame >= Main.projFrames[Projectile.type])
-                        Projectile.frame = 0;
+
+                    Player victim = Main.player[BittenPlayer];
+                    if (victim.active && !victim.ghost && !victim.dead
+                        && (Projectile.Distance(victim.Center) < 160 || victim.whoAmI != Main.myPlayer)
+                        && victim.FargoSouls().MashCounter < 20)
+                    {
+                        victim.AddBuff(ModContent.BuffType<GrabbedBuff>(), 2);
+                        victim.velocity = Vector2.Zero;
+                        Projectile.Center = victim.Center;
+                    }
+                    else
+                    {
+                        BittenPlayer = -1;
+                        Projectile.netUpdate = true;
+                    }
+                }
+
+                if (FargoSoulsUtil.BossIsAlive(ref EModeGlobalNPC.wallBoss, NPCID.WallofFlesh)
+                    && Math.Abs(Projectile.Center.X - Main.npc[EModeGlobalNPC.wallBoss].Center.X) < 50)
+                {
+                    Projectile.localAI[2] = 1; //chain dies when wall moves over it
+                }
+
+                if (Projectile.velocity != Vector2.Zero)
+                {
+                    Projectile.rotation = Projectile.velocity.ToRotation();
+
+                    if (++Projectile.frameCounter > 6 * (Projectile.extraUpdates + 1))
+                    {
+                        Projectile.frameCounter = 0;
+                        if (++Projectile.frame >= Main.projFrames[Projectile.type])
+                            Projectile.frame = 0;
+                    }
                 }
             }
         }
@@ -167,7 +194,7 @@ namespace FargowiltasSouls.Content.Projectiles.Masomode
             {
                 Texture2D texture = TextureAssets.Chain12.Value;
                 Vector2 position = Projectile.Center;
-                Vector2 mountedCenter = new(Projectile.localAI[0], Projectile.localAI[1]);
+                Vector2 mountedCenter = OriginPosition;
                 Rectangle? sourceRectangle = new Rectangle?();
                 Vector2 origin = new(texture.Width * 0.5f, texture.Height * 0.5f);
                 float num1 = texture.Height;
@@ -218,10 +245,27 @@ namespace FargowiltasSouls.Content.Projectiles.Masomode
                 Main.EntitySpriteDraw(texture2D13, value4 + Projectile.Size / 2f - Main.screenPosition + new Vector2(0, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color27, num165, origin2, Projectile.scale, effects, 0);
             }*/
 
+            /*
+            if (Telegraphing)
+            {
+                Color color = Color.DeepPink;
+                Asset<Texture2D> line = TextureAssets.Extra[178];
+                float opacity = 0.9f;
+                float rot = Projectile.rotation;
+                Main.EntitySpriteDraw(line.Value, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), null, color * opacity, rot, new Vector2(0, line.Height() * 0.5f), new Vector2(0.47f, Projectile.scale * 7), SpriteEffects.None);
+            }
+            */
+
             Main.EntitySpriteDraw(texture2D13, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), Projectile.GetAlpha(lightColor), Projectile.rotation, origin2, Projectile.scale, effects, 0);
 
             //spriteBatch.End(); spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Main.GameViewMatrix.ZoomMatrix);
             return false;
+        }
+
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        {
+            if (Projectile.hide)
+                overWiresUI.Add(index);
         }
     }
 }
