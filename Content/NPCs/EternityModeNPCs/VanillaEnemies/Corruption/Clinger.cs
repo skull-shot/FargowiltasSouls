f5a32ca979a38735780d709138a6bb010626faeb
schmoovi <romandark1@gmail.com>
2025-07-29 19:05:13 -0500

    World Feeder: No longer spawn in Pre-Hardmode with modified loot

---
+++ Content/NPCs/EternityModeNPCs/VanillaEnemies/Corruption/Clinger.cs
@@ -0,0 +1,11 @@
+﻿using FargowiltasSouls.Core.Globals;
+using FargowiltasSouls.Core.NPCMatching;
+using Terraria.ID;
+
+namespace FargowiltasSouls.Content.NPCs.EternityModeNPCs.VanillaEnemies.Corruption
+{
+    public class Clinger : EModeNPCBehaviour
+    {
+        public override NPCMatcher CreateMatcher() => new NPCMatcher().MatchType(NPCID.Clinger);
+    }
+}
