1447349500d427636a13647921b13c5e25152164
JavyzTaken <jacklukas@outlook.com>
2025-08-03 22:24:12 +0200

    ambrosia damage 22% -> 20%, removed melee speed

--- Content/Buffs/Souls/AmbrosiaBuff.cs
+++ Content/Buffs/Souls/AmbrosiaBuff.cs
@@ -13,8 +13,8 @@ namespace FargowiltasSouls.Content.Buffs.Souls
         public override void Update(Player player, ref int buffIndex)
         {
             player.endurance += 0.15f;
-            player.GetDamage(DamageClass.Generic) += 0.22f;
-            player.GetAttackSpeed(DamageClass.Melee) += 0.22f;
+            player.GetDamage(DamageClass.Generic) += 0.2f;
+            //player.GetAttackSpeed(DamageClass.Melee) += 0.22f;
             player.FargoSouls().Ambrosia = true;
             player.FargoSouls().MinionCrits = true;
         }
