6d6fbe99da840d9cf923e5d43d4edea82bffb6b4
schmoovi <romandark1@gmail.com>
2025-08-04 03:17:53 -0500

    Nymph's Perfume: Dropped heart replaced with a lifestealing heart streak on hits, which always heals 20 health.

---
+++ Content/Projectiles/Accessories/BionomicCluster/NymphHeart.cs
@@ -0,0 +1,47 @@
+﻿using FargowiltasSouls.Common.Graphics.Particles;
+using FargowiltasSouls.Content.Items;
+using Luminance.Core.Graphics;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Content.Projectiles.Accessories.Souls
+{
+    public class NymphHeart : ModProjectile
+    {
+        public override string Texture => FargoSoulsUtil.EmptyTexture;
+        public override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.VampireHeal);
+            Projectile.aiStyle = -1;
+            Projectile.timeLeft = 900;
+        }
+
+        public override void AI()
+        {
+            Player player = FargoSoulsUtil.PlayerExists(Projectile.ai[0]);
+            Vector2 dist = player.Center - Projectile.Center;
+            dist.Normalize();
+            dist *= 2;
+            Projectile.velocity = (Projectile.velocity * 15f + dist) / 16f;
+
+            if (Projectile.Distance(player.Center) < 16)
+            {
+                player.FargoSouls().HealPlayer(20);
+                FargoGlobalItem.OnRetrievePickup(player);
+                SoundEngine.PlaySound(SoundID.Grab, Projectile.Center);
+                Projectile.Kill();
+            }
+            if (Projectile.timeLeft % 20 == 0 && Projectile.timeLeft != 0)
+            {
+                Color color = Color.Lerp(Color.Red, Color.DeepPink, Main.rand.NextFloat(0.1f, 0.9f));
+                Color f = Color.Lerp(Color.Lerp(Color.Red, Color.DeepPink, Main.rand.NextFloat(0.1f, 0.9f)), Color.Transparent, 0.25f);
+                Particle p = new HeartParticle(Projectile.Center + Main.rand.NextVector2Circular(10, 10), Projectile.velocity*1.5f, f, 1f, 50, false);
+                p.Rotation = p.Velocity.ToRotation();
+                p.Spawn();
+            }
+        }
+    }
+}
