7faa5426fbbb5c4ed4dd8471f3ce1dd0b8c57d6e
JavyzTaken <jacklukas@outlook.com>
2025-08-08 13:15:37 +0200

    Balance tooltip config now has 3 options: Reworks, All, None. Reworks by default Fixed conditional on Space Gun lock tooltip

---
+++ Core/BalanceTooltipSetting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FargowiltasSouls.Core
+{
+    public enum BalanceTooltipSetting
+    {
+        All,
+        Reworks,
+        None
+    }
+}
