aa6881e4da3b9a9621b59cf4a6be1e019038fd60
Habble53 <87115317+Habble53@users.noreply.github.com>
2025-08-23 07:20:48 +0300

    Oops

--- Content/Projectiles/Eternity/Bosses/DukeFishron/FishronRitual.cs
+++ Content/Projectiles/Eternity/Bosses/DukeFishron/FishronRitual.cs
@@ -26,7 +26,6 @@ namespace FargowiltasSouls.Content.Projectiles.Eternity.Bosses.DukeFishron
             Projectile.tileCollide = false;
             Projectile.timeLeft = 300;
             Projectile.alpha = 255;
-            CooldownSlot = ImmunityCooldownID.Bosses;
             if (WorldSavingSystem.MasochistModeReal) //Fargowiltas.Instance.MasomodeEXLoaded)
                 Projectile.extraUpdates = 1;
         }
@@ -146,7 +145,7 @@ namespace FargowiltasSouls.Content.Projectiles.Eternity.Bosses.DukeFishron
 
         public override bool? CanDamage()
         {
-            return true;
+            return false;
         }
 
         public override Color? GetAlpha(Color lightColor)
