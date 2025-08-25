61e3d83ccd44c562e7c1be412c2ab1c859238aad
JavyzTaken <jacklukas@outlook.com>
2025-08-25 22:44:16 +0200

    Gem Staff eternity effects: -Amethyst Staff: Bolt is faster and bounces once -Topaz Staff: Critical strikes cause an explosion -Sapphire Staff: Rapidly fires bolts in a spread -Emerald Staff: Fires a large bolt with strong knockback and pierce -Amber Staff: Bolt boomerangs back -Ruby Staff: Bolt splits in half after a duration -Diamond Staff unchanged

---
+++ Content/Projectiles/Weapons/TopazBoltExplosion.cs
@@ -0,0 +1,55 @@
+﻿using FargowiltasSouls.Assets.Textures;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Content.Projectiles.Weapons
+{
+    public class TopazBoltExplosion : ModProjectile
+    {
+        public override string Texture => FargoSoulsUtil.EmptyTexture;
+        public override void SetStaticDefaults()
+        {
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 120;
+            Projectile.height = 120;
+            Projectile.aiStyle = 0;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 30;
+            Projectile.tileCollide = false;
+            Projectile.light = 0.75f;
+            Projectile.ignoreWater = true;
+            //Projectile.extraUpdates = 1;
+            AIType = ProjectileID.Bullet;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.FargoSouls().DeletionImmuneRank = 2;
+
+        }
+        public override void OnSpawn(IEntitySource source)
+        {
+            SoundEngine.PlaySound(SoundID.Item54, Projectile.Center);
+        }
+        public override void AI()
+        {
+            Projectile.velocity = Vector2.Zero;
+            if (Projectile.localAI[2] == 0)
+            {
+                Projectile.localAI[2] = 1;
+                for (int i = 0; i < 40; i++)
+                {
+                    int d = Dust.NewDust(Projectile.Center + Main.rand.NextVector2Circular(Projectile.width / 3, Projectile.height / 3), 1, 1, DustID.GemTopaz);
+                    Main.dust[d].velocity = Main.rand.NextFloat(1f, 4f) * (Main.dust[d].position - Projectile.Center).SafeNormalize(Vector2.UnitX);
+                    Main.dust[d].noGravity = true;
+                }
+            }
+        }
+    }
+}
