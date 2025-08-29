109cd528e17729106a272bedd106dd04af8e5a31
schmoovi <romandark1@gmail.com>
2025-08-29 01:12:05 -0500

    fixed king slime death animation debug text

--- Content/NPCs/ReleasedMutant.cs
+++ Content/NPCs/ReleasedMutant.cs
@@ -113,7 +113,7 @@ namespace FargowiltasSouls.Content.NPCs
 
         public override void AI()
         {
-            Main.NewText(NPC.timeLeft);
+            //Main.NewText(NPC.timeLeft);
 
             DrawOffsetY = -2;
             
