d52b349d4e6078b31c65e95b04137f2fdd3211fa
schmoovi <romandark1@gmail.com>
2025-09-05 03:36:36 -0500

    Fixed Eternal Flame failing to draw when equipped in vanity slots
    Fixed Eternal Flame skipping a frame when animating
    Fixed Eternal Flame sprite jittering while moving

--- Content/PlayerDrawLayers/MutantMaskDrawLayer.cs
+++ Content/PlayerDrawLayers/MutantMaskDrawLayer.cs
@@ -10,8 +10,8 @@ namespace FargowiltasSouls.Content.PlayerDrawLayers
     public class MutantMaskDrawLayer : PlayerDrawLayer
     {
         public int maxFrames = 4;
-        public int frame = 1;
-        public int framecounter;    
+        public int frame = 0;
+        public int framecounter;
 
         public override Position GetDefaultPosition() => new AfterParent(Terraria.DataStructures.PlayerDrawLayers.Head);
 
@@ -21,22 +21,33 @@ namespace FargowiltasSouls.Content.PlayerDrawLayers
             if (drawInfo.shadow != 0)
                 return false;
 
-            if (player.armor[0].type != ModContent.ItemType<MutantMask>())
+            if (player.armor[0].type != ModContent.ItemType<MutantMask>() && player.armor[10].type != ModContent.ItemType<MutantMask>())
                 return false;
+
+            if (player.head != EquipLoader.GetEquipSlot(Mod, "MutantMask", EquipType.Head))
+                return false;
+
             return true;
         }
 
         public override bool IsHeadLayer => true;
 
         protected override void Draw(ref PlayerDrawSet drawInfo)
-        {        
+        {
             Player drawPlayer = drawInfo.drawPlayer;
 
-            //yes, all of that is really neccessary to emulate head bobbing.
-            Vector2 offset = (drawPlayer.gravDir > 0 ? (drawInfo.drawPlayer.direction == 1 ? new Vector2(-12, -16) : new Vector2(-8, -16)) : (drawInfo.drawPlayer.direction == 1 ? new Vector2(-12, 8) : new Vector2(-8, 8))) + drawPlayer.headPosition + drawInfo.headVect + Main.OffsetsPlayerHeadgear[drawPlayer.bodyFrame.Y / drawPlayer.bodyFrame.Height] * drawPlayer.gravDir;
-            Vector2 drawPosition = drawInfo.Position - Main.screenPosition;
-            drawPosition += offset;
-            drawPosition = new Vector2((int)drawPosition.X, (int)drawPosition.Y);
+            Rectangle bodyFrame = drawInfo.drawPlayer.bodyFrame;
+
+            Vector2 Position = drawInfo.helmetOffset +
+                new Vector2((int)(drawInfo.Position.X - Main.screenPosition.X - (float)(drawInfo.drawPlayer.bodyFrame.Width / 2) +
+                (float)(drawInfo.drawPlayer.width / 2)),
+                (int)(drawInfo.Position.Y - Main.screenPosition.Y +
+                (float)drawInfo.drawPlayer.height -
+                (float)drawInfo.drawPlayer.bodyFrame.Height + 4f)) +
+                drawInfo.drawPlayer.headPosition +
+                drawInfo.headVect +
+                new Vector2(drawPlayer.direction == -1 ? 2 : -2, drawPlayer.gravDir == 1 ? -6 : 16) +
+                Main.OffsetsPlayerHeadgear[drawPlayer.bodyFrame.Y / drawPlayer.bodyFrame.Height] * drawPlayer.gravDir;
 
             var FlameTexture = ModContent.Request<Texture2D>("FargowiltasSouls/Content/Items/Armor/MutantMask", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value;
 
@@ -45,13 +56,14 @@ namespace FargowiltasSouls.Content.PlayerDrawLayers
                 framecounter = 0;
                 frame++;
                 if (frame >= maxFrames)
-                    frame = 1;
+                    frame = 0;
             }
             Rectangle flameframe = new(0, (FlameTexture.Height / maxFrames) * frame, FlameTexture.Width, FlameTexture.Height / maxFrames);
-            DrawData data = new(FlameTexture, drawPosition.Floor(), flameframe, drawInfo.colorArmorBody, drawInfo.drawPlayer.bodyRotation, flameframe.Size() * 0.5f, 1.2f, drawInfo.playerEffect, 0f);
-            data.shader = drawInfo.cHead;
-            drawInfo.DrawDataCache.Add(data);
 
+            DrawData item = new DrawData(FlameTexture, Position, flameframe, Color.White, drawInfo.drawPlayer.headRotation, flameframe.Size() / 2f, 1.2f, drawInfo.playerEffect);
+            item.shader = drawInfo.cHead;
+            drawInfo.DrawDataCache.Add(item);
+            return;
         }
     }
 }
