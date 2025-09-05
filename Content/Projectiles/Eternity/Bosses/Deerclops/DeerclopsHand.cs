b2300629a5e1808ac4174264358083d9d2ac17de
terrynmuse <darius.loyhrs@hotmail.com>
2025-09-04 22:50:20 -0700

    adjusted deer hand visuals

--- Content/Projectiles/Eternity/Bosses/Deerclops/DeerclopsHand.cs
+++ Content/Projectiles/Eternity/Bosses/Deerclops/DeerclopsHand.cs
@@ -147,10 +147,12 @@ namespace FargowiltasSouls.Content.Projectiles.Eternity.Bosses.Deerclops
             for (int i = 0; i < ProjectileID.Sets.TrailCacheLength[Projectile.type]; i++)
             {
                 Color color27 = Color.LightBlue * Projectile.Opacity * 0.75f;
-                color27 *= (float)(ProjectileID.Sets.TrailCacheLength[Projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[Projectile.type];
+                float ratio = (float)(ProjectileID.Sets.TrailCacheLength[Projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[Projectile.type];
+                color27 *= ratio * ratio;
+                float scale = Projectile.scale * 1.1f * ratio;
                 Vector2 value4 = Projectile.oldPos[i];
                 float num165 = Projectile.oldRot[i];
-                Main.EntitySpriteDraw(texture2D13, value4 + Projectile.Size / 2f - Main.screenPosition + new Vector2(0, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color27, num165, origin2, Projectile.scale * 1.1f, effects, 0);
+                Main.EntitySpriteDraw(texture2D13, value4 + Projectile.Size / 2f - Main.screenPosition + new Vector2(0, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color27, num165, origin2, scale, effects, 0);
             }
 
             Main.EntitySpriteDraw(texture2D13, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color26, Projectile.rotation, origin2, Projectile.scale, effects, 0);
