56c5345a9c80b51c9ee52923dd73519b024c90ca
Liam <leeleegill20@gmail.com>
2025-09-19 15:50:20 -0400

    evil water no longer chnages color, increased dust range, reduced frequency

--- Core/ModPlayers/EnvironmentalEffects.cs
+++ Core/ModPlayers/EnvironmentalEffects.cs
@@ -190,11 +190,11 @@ namespace FargowiltasSouls.Core.ModPlayers
         {
             //search around player for water, add dust
             Vector2 playerPos = Player.Center;
-            int radius = 500;
+            int radius = 1000;
 
-            for (int x = -radius; x <= radius; x++)
+            for (int x = -radius; x <= radius; x += 2)
             {
-                for (int y = -radius; y <= radius; y++)
+                for (int y = -radius; y <= radius; y += 2)
                 {
                     int xPosition = (int)(x + playerPos.X / 16.0f);
                     int yPosition = (int)(y + playerPos.Y / 16.0f);
