1faf2d0620a029c4cef2032cdd07c818f22f5144
schmoovi <romandark1@gmail.com>
2025-09-30 18:52:29 -0500

    Nekomi Armor: Now uses Demoninte/Crimtane bars and Pink Gel in recipe

--- Content/Items/Armor/Nekomi/NekomiHood.cs
+++ Content/Items/Armor/Nekomi/NekomiHood.cs
@@ -162,6 +162,8 @@ namespace FargowiltasSouls.Content.Items.Armor.Nekomi
         {
             CreateRecipe()
             .AddIngredient(ItemID.Silk, 10)
+            .AddRecipeGroup("FargowiltasSouls:AnyDemoniteBar", 6)
+            .AddIngredient(ItemID.PinkGel, 5)
             .AddIngredient(ModContent.ItemType<DeviatingEnergy>(), 5)
             .AddTile(TileID.Loom)
 
