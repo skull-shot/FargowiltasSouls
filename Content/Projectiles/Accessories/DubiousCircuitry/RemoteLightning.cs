7970d77297f3361502963a3b7b81b56d9369f91e
Habble53 <87115317+Habble53@users.noreply.github.com>
2025-10-02 18:23:42 +0300

    Fix

--- Content/Projectiles/Accessories/DubiousCircuitry/RemoteLightning.cs
+++ Content/Projectiles/Accessories/DubiousCircuitry/RemoteLightning.cs
@@ -67,6 +67,8 @@ namespace FargowiltasSouls.Content.Projectiles.Accessories.DubiousCircuitry
         public override void ModifyHitPlayer(Player target, ref Player.HurtModifiers modifiers)
         {
             modifiers.SourceDamage *= 0f;
+            if (target.HasEffect<RemoteControlDR>() && !target.HasBuff<SuperchargedBuff>())
+                modifiers.Knockback *= 0f;
             //doing it like this bc scaled projectile damage doesnt work?
             if (Main.masterMode) modifiers.SourceDamage.Flat += 270;
             else if (Main.expertMode) modifiers.SourceDamage.Flat += 180;
