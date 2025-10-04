bd3b957606d5fb387280028ebc43d61982825d23
JavyzTaken <jacklukas@outlook.com>
2025-10-04 20:11:25 +0200

    Added Effect Toggler and Active Skills items, crafted with 1 Iron Bar, which opens the corresponding menus
    -proper sprites for menu icons

--- Content/UI/Elements/EffectTogglerItem.cs
+++ Content/UI/Elements/EffectTogglerItem.cs
@@ -1,4 +1,5 @@
-﻿using FargowiltasSouls.Assets.Textures;
+﻿using Fargowiltas.Content.UI;
+using FargowiltasSouls.Assets.Textures;
 using FargowiltasSouls.Content.Bosses.MutantBoss;
 using FargowiltasSouls.Content.Items.Placables.Trophies;
 using Microsoft.Xna.Framework;
@@ -9,10 +10,10 @@ using Terraria.ModLoader;
 
 namespace FargowiltasSouls.Content.UI.Elements
 {
-    // Exists to be displayed as an item icon in the Toggler UI when inflicted with Mutant's Presence.
-    public class OncomingMutantItem : ModItem
+    // Exists to be displayed as an item icon in the Toggler UI
+    public class EffectTogglerItem : ModItem
     {
-        public override string Texture => FargoAssets.GetAssetString("UI", "OncomingMutant");
+        public override string Texture => FargoAssets.GetAssetString("UI", "EffectTogglerItem");
 
         public override void SetStaticDefaults()
         {
@@ -20,15 +21,20 @@ namespace FargowiltasSouls.Content.UI.Elements
         }
         public override void SetDefaults()
         {
-            Item.useAnimation = 30;
-            Item.useTime = 30;
+            Item.useAnimation = 4;
+            Item.useTime = 4;
             Item.useStyle = ItemUseStyleID.HoldUp;
             Item.consumable = false;
         }
         public override bool? UseItem(Player player)
         {
-            FargoSoulsUtil.SpawnBossNetcoded(player, ModContent.NPCType<MutantBoss>());
+            CombinedUI.ToggleUI<SoulToggler>();
             return true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().AddIngredient(RecipeGroupID.IronBar).AddTile(TileID.Anvils).Register();
         }
     }
 }
