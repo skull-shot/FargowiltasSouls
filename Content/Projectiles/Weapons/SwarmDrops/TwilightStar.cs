65867f436e01406aaeecf2b51a93766139518637
Beetle <jiji>
2025-10-07 22:00:53 -0400

    Dark Mage swarm weapon progress

--- Content/Projectiles/Weapons/SwarmDrops/TwilightStar.cs
+++ Content/Projectiles/Weapons/SwarmDrops/TwilightStar.cs
@@ -15,9 +15,9 @@ using Terraria.ModLoader;
 
 namespace FargowiltasSouls.Content.Projectiles.Weapons.SwarmDrops
 {
-    public class RunicSigil : ModProjectile
+    public class TwilightStar : ModProjectile
     {
-        public override string Texture => "Terraria/Images/Extra_240";
+        public override string Texture => "Terraria/Images/Projectile_79";
 
         #region References
         /*
@@ -33,21 +33,19 @@ namespace FargowiltasSouls.Content.Projectiles.Weapons.SwarmDrops
 
         public override void SetStaticDefaults()
         {
-            Main.projFrames[Type] = 8;
-            ProjectileID.Sets.TrailCacheLength[Type] = 8;
-            ProjectileID.Sets.TrailingMode[Type] = 3;
+
         }
 
         public override void SetDefaults()
         {
-            Projectile.width = 60;
-            Projectile.height = 90;
+            Projectile.width = 52;
+            Projectile.height = 52;
             Projectile.DamageType = DamageClass.Magic;
             Projectile.friendly = true;
             Projectile.tileCollide = false;
             Projectile.penetrate = -1;
             Projectile.Opacity = 0.9f;
-            Projectile.scale = 0.5f;
+            Projectile.light = 0.6f;
         }
 
         public override void AI()
@@ -55,8 +53,6 @@ namespace FargowiltasSouls.Content.Projectiles.Weapons.SwarmDrops
             Player player = Main.player[Projectile.owner];
             if (!player.active || player.dead || player.ghost)
                 Projectile.Kill();
-            float rot = (Main.MouseWorld - Projectile.Center).ToRotation();
-            Projectile.rotation = rot;
 
             timer++;
             Frame();
@@ -65,30 +61,37 @@ namespace FargowiltasSouls.Content.Projectiles.Weapons.SwarmDrops
                 case 0:
                     if (!player.channel)
                         Projectile.Kill();
-                    if (timer > 45)
+                    if (timer > 20)
                     {
-                        SoundEngine.PlaySound(SoundID.Item103, Projectile.Center);
+                        SoundEngine.PlaySound(SoundID.Item29 with { Pitch = -1f, Volume = 0.5f }, Projectile.Center);
                         state = 1;
                     }
                     break;
                 case 1:
-                    if (!player.channel && player.HeldItem.type == ModContent.ItemType<RunicBook>() && !player.controlUseItem)
+                    if (!player.channel && player.HeldItem.type == ModContent.ItemType<TwilightTome>() && !player.controlUseItem)
                     {
-                        SoundEngine.PlaySound(SoundID.Item9, Projectile.Center);
+                        SoundEngine.PlaySound(SoundID.Item43 with { Pitch = -0.5f }, Projectile.Center);
                         timer = 0;
                         state = 2;
                         return;
                     }
                     else
                     {
-                        float randRot = Main.rand.NextFloat(0, MathHelper.TwoPi);
-                        new SparkParticle(Projectile.Center + 5 * Vector2.UnitY + 20 * Vector2.UnitX.RotatedBy(randRot), -4 * Vector2.UnitX.RotatedBy(randRot), Color.Purple, 0.2f, 3).Spawn();
+                        if (timer % 30 == 0) // occasional twinkle
+                            for (int i = 0; i < 3; i++)
+                            {
+                                float randRot = Main.rand.NextFloat(0, MathHelper.TwoPi);
+                                new SparkParticle(Projectile.Center + 5 * Vector2.UnitX.RotatedBy(randRot), 2 * Vector2.UnitX.RotatedBy(randRot), Color.SkyBlue, 0.2f, 6).Spawn();
+                                new SmallSparkle(Projectile.Center + 5 * Vector2.UnitX.RotatedBy(randRot), 2 * Vector2.UnitX.RotatedBy(randRot), Color.SkyBlue, 1f, 7).Spawn();
+                            }
                     }
                     break;
                 case 2:
-                    if (timer > 3) // let unformed portals despawn
+                    if (timer > 3) // let unformed stars despawn
                     {
+                        // scale projectile count with star number
                         projCount = 2 * player.ownedProjectileCounts[Projectile.type];
+
                         timer = 0;
                         state = 3;
                     }
@@ -96,22 +99,25 @@ namespace FargowiltasSouls.Content.Projectiles.Weapons.SwarmDrops
                 case 3:
                     if (timer % 15 == 14 && projCount > 0)
                     {
+                        float rot = (Main.MouseWorld - Projectile.Center).ToRotation();
                         Vector2 vel = new Vector2(10f, 0f).RotatedBy(rot);
-                        FargoSoulsUtil.DustRing(Projectile.Center, 20, DustID.PinkTorch, 3f, scale: 1.5f);
-                        SoundEngine.PlaySound(SoundID.NPCDeath52, Projectile.Center);
+                        FargoSoulsUtil.DustRing(Projectile.Center, 20, DustID.HallowSpray, 3f, scale: 1.5f);
+                        SoundEngine.PlaySound(SoundID.Item75 with { Pitch = -0.5f }, Projectile.Center);
 
                         // Random spread
                         float spread = MathHelper.Pi / 24;
                         float randRot = Main.rand.NextFloat(-spread, spread);
 
-                        Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.Center, vel.RotatedBy(randRot),
-                            ModContent.ProjectileType<RunicBlast>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+                        if (FargoSoulsUtil.HostCheck)
+                            Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.Center, vel.RotatedBy(randRot),
+                                ModContent.ProjectileType<TwilightStarSpawn>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
 
                         projCount--;
                         if (projCount <= 0)
                         {
                             state = 4;
                         }
+                        Projectile.netUpdate = true;
                     }
                     break;
                 case 4:
@@ -129,12 +135,7 @@ namespace FargowiltasSouls.Content.Projectiles.Weapons.SwarmDrops
 
         void Frame()
         {
-            if (timer % 4 == 0)
-            {
-                Projectile.frame++;
-                if (Projectile.frame >= Main.projFrames[Type])
-                    Projectile.frame = 0;
-            }
+            
         }
 
         public override bool? CanHitNPC(NPC target)
@@ -144,11 +145,21 @@ namespace FargowiltasSouls.Content.Projectiles.Weapons.SwarmDrops
 
         public override bool PreDraw(ref Color lightColor)
         {
-            float opac = 1;
+            float opac = Projectile.Opacity;
             if (state == 0)
-                opac = timer / 60;
+                opac = timer / 20;
 
-            FargoSoulsUtil.GenericProjectileDraw(Projectile, lightColor * opac, rotation: 0);
+            Texture2D text = TextureAssets.Projectile[16].Value;
+            Rectangle frame = text.Frame();
+
+            float count = 10;
+            for (int i = 0; i < count; i++)
+            {
+                float scale = (1 - (i / count));
+                Color c = Color.SkyBlue * opac * (i/count);
+                Main.EntitySpriteDraw(text, Projectile.position - Main.screenPosition + frame.Size() / 2, frame, c, 0, frame.Size() / 2, Projectile.scale * scale, SpriteEffects.None);
+            }
+            //FargoSoulsUtil.GenericProjectileDraw(Projectile, lightColor * opac, rotation: 0);
             return false;
         }
     }
