c592122c9cdd2db7423d75423187728f0f4f9c56
Beetle <jiji>
2025-10-07 22:09:27 -0400

    added proj delete rank checks to proj absorbtion

--- Content/Projectiles/Eternity/Enemies/Vanilla/OOA/OOAForcefield.cs
+++ Content/Projectiles/Eternity/Enemies/Vanilla/OOA/OOAForcefield.cs
@@ -163,7 +163,7 @@ namespace FargowiltasSouls.Content.Projectiles.Eternity.Enemies.Vanilla.OOA
 
             float distance = 2f * 24;
 
-            Main.projectile.Where(x => x.active && x.friendly && !FargoSoulsUtil.IsSummonDamage(x, false)).ToList().ForEach(x =>
+            Main.projectile.Where(x => x.active && x.friendly && x.FargoSouls().DeletionImmuneRank == 0 && !FargoSoulsUtil.IsSummonDamage(x, false)).ToList().ForEach(x =>
             {
                 if (Vector2.Distance(x.Center, Projectile.Center) <= distance)
                 {
