3a51ca481da2ee41693dedf649b9db990c41d257
schmoovi <romandark1@gmail.com>
2025-10-20 01:08:15 -0500

    Ghost:
    - No longer has an aura of Cursed, a chance to decrease damage taken to 1, a chance to spawn in hordes of 4, or further increased life in Hardmode
    - Now only triggers invulnerable phase while the player actively looks at them instead of away from them, but now has slowed movement during invulnerable phase instead of no movement, and will enter vulnerable phase if sufficiently close enough to the player to jump them
    - Phases should no longer occur while ghosts are too far away, and added visual indication for switching between phases

--- Content/NPCs/EternityModeNPCs/VanillaEnemies/Cavern/Ghost.cs
+++ Content/NPCs/EternityModeNPCs/VanillaEnemies/Cavern/Ghost.cs
@@ -1,8 +1,10 @@
-﻿using FargowiltasSouls.Core.Globals;
+﻿using System;
+using System.Linq;
+using FargowiltasSouls.Core.Globals;
 using FargowiltasSouls.Core.NPCMatching;
 using Microsoft.Xna.Framework;
-using System.Linq;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ID;
 
 namespace FargowiltasSouls.Content.NPCs.EternityModeNPCs.VanillaEnemies.Cavern
@@ -13,39 +15,53 @@ namespace FargowiltasSouls.Content.NPCs.EternityModeNPCs.VanillaEnemies.Cavern
         public override void SetDefaults(NPC npc)
         {
             npc.knockBackResist = 0f;
-            if (Main.hardMode)
-                npc.lifeMax = (int)(npc.lifeMax * 1.25f);
+            //if (Main.hardMode)
+                //npc.lifeMax = (int)(npc.lifeMax * 1.25f);
         }
         public override void OnFirstTick(NPC npc)
         {
             base.OnFirstTick(npc);
 
-            if (Main.rand.NextBool(5) && npc.FargoSouls().CanHordeSplit)
-                EModeGlobalNPC.Horde(npc, 3);
+            //if (Main.rand.NextBool(5) && npc.FargoSouls().CanHordeSplit)
+                //EModeGlobalNPC.Horde(npc, 3);
         }
-
+        public bool MadeDustRingForTrans;
         public override void AI(NPC npc)
         {
             base.AI(npc);
-
-            EModeGlobalNPC.Aura(npc, 100, BuffID.Cursed, false, 20);
+            //EModeGlobalNPC.Aura(npc, 100, BuffID.Cursed, false, 20);
             npc.dontTakeDamage = false;
-            if (npc.HasPlayerTarget && Main.player[npc.target].Alive() && Main.player[npc.target].direction != Main.player[npc.target].HorizontalDirectionTo(npc.Center))
+            Player target = Main.player[npc.target];
+            if (Collision.CanHitLine(npc.Center, 0, 0, target.Center, 0, 0) && Math.Abs(npc.Center.X - target.Center.X) <= 16*40 && Math.Abs(npc.Center.Y - target.Center.Y) <= 16*20)
             {
-                npc.dontTakeDamage = true;
-                npc.velocity *= 0f;
-                npc.Opacity = MathHelper.Lerp(npc.Opacity, 0.4f, 0.05f);
-            }
-            else
-            {
-                npc.Opacity = MathHelper.Lerp(npc.Opacity, 1f, 0.05f);
-                npc.position += npc.velocity * 1f;
+                if (npc.HasPlayerTarget && target.Alive() && target.direction == Math.Sign(npc.Center.X - target.Center.X) && npc.Distance(target.Center) >= 16*3)
+                {
+                    if (!MadeDustRingForTrans)
+                    {
+                        FargoSoulsUtil.DustRing(npc.Center + npc.velocity, 12, DustID.PortalBolt, 4, scale: 1.5f);
+                        MadeDustRingForTrans = true;
+                    }
+                    npc.dontTakeDamage = true;
+                    npc.position -= npc.velocity / 2; //halved speed
+                    npc.Opacity = MathHelper.Lerp(npc.Opacity, 0.4f, 0.1f);
+                }
+                else
+                {
+                    if (MadeDustRingForTrans)
+                    {
+                        FargoSoulsUtil.DustRing(npc.Center + npc.velocity, 12, DustID.PortalBolt, 8, scale: 1.5f);
+                        MadeDustRingForTrans = false;
+                        SoundEngine.PlaySound(SoundID.NPCHit36 with {Pitch = 0.5f, MaxInstances = 1}, npc.Center);
+                    }
+                    npc.Opacity = MathHelper.Lerp(npc.Opacity, 1f, 0.1f);
+                    npc.position += npc.velocity; //doubled speed
+                }
             }
         }
         public override void ModifyHitByAnything(NPC npc, Player player, ref NPC.HitModifiers modifiers)
         {
-            if (Main.rand.NextBool(3))
-                modifiers.SetMaxDamage(1);
+            //if (Main.rand.NextBool(3))
+                //modifiers.SetMaxDamage(1);
         }
     }
 }
