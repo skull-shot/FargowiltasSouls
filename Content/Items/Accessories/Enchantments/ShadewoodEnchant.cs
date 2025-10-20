b51263dce437aad9bb0b7e9b4c18f90c89137bf7
JavyzTaken <jacklukas@outlook.com>
2025-10-20 21:50:41 +0200

    Shadewood mini-rework: You are surrounded by an aura of blood Having enemies in the aura siphons blood power from them With full blood power, hitting enemies consumes it to cause a splatter of blood

--- Content/Items/Accessories/Enchantments/ShadewoodEnchant.cs
+++ Content/Items/Accessories/Enchantments/ShadewoodEnchant.cs
@@ -1,10 +1,13 @@
 ﻿using Fargowiltas.Content.Items.Tiles;
+using FargowiltasSouls.Assets.Textures;
 using FargowiltasSouls.Content.Buffs.Souls;
 using FargowiltasSouls.Content.Items.Accessories.Forces;
 using FargowiltasSouls.Content.Projectiles.Accessories.Souls;
+using FargowiltasSouls.Content.UI.Elements;
 using FargowiltasSouls.Core.AccessoryEffectSystem;
 using FargowiltasSouls.Core.Toggler.Content;
 using Microsoft.Xna.Framework;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -50,18 +53,24 @@ namespace FargowiltasSouls.Content.Items.Accessories.Enchantments
         }
         public override int DamageTooltip(out DamageClass damageClass, out Color? tooltipColor, out int? scaling)
         {
+            Player player = Main.LocalPlayer;
+            scaling = (int)((player.HeldItem.damage + player.FindAmmo([player.HeldItem.useAmmo]).damage) * player.ActualClassDamage(player.HeldItem.DamageType));
+            if (scaling < 0)
+                scaling = 0;
+
+            scaling = (int)ShadewoodEffect.GetScaledDamage(player, (float)scaling);
+
             damageClass = DamageClass.Melee;
             tooltipColor = null;
-            scaling = null;
-            return ShadewoodEffect.BaseDamage(Main.LocalPlayer);
+            return 100;
         }
     }
     public class ShadewoodEffect : AccessoryEffect
     {
-
         public override Header ToggleHeader => Header.GetHeader<TimberHeader>();
         public override int ToggleItemType => ModContent.ItemType<ShadewoodEnchant>();
         public static int Range(Player player, bool forceEffect) => (int)((forceEffect ? 400f : 200f) * (1f + player.FargoSouls().AuraSizeBonus));
+        public static int MaxCharge = 180;
         public override void PostUpdateEquips(Player player)
         {
             FargoSoulsPlayer modPlayer = player.FargoSouls();
@@ -70,7 +79,7 @@ namespace FargowiltasSouls.Content.Items.Accessories.Enchantments
                 return;
             bool forceEffect = modPlayer.ForceEffect<ShadewoodEnchant>();
             int dist = Range(player, forceEffect);
-
+            bool found = false;
             for (int i = 0; i < Main.maxNPCs; i++)
             {
                 NPC npc = Main.npc[i];
@@ -79,12 +88,29 @@ namespace FargowiltasSouls.Content.Items.Accessories.Enchantments
                     Vector2 npcComparePoint = FargoSoulsUtil.ClosestPointInHitbox(npc, player.Center);
                     if (player.Distance(npcComparePoint) < dist && (forceEffect || Collision.CanHitLine(player.Center, 0, 0, npcComparePoint, 0, 0)))
                     {
-                        //if (!player.HasEffect<TimberEffect>())
-                        npc.AddBuff(ModContent.BuffType<SuperBleedBuff>(), 60);
+                        if (!found)
+                        {
+                            found = true;
+                            modPlayer.ShadewoodCharge += 1;
+                        }
+                        else
+                            modPlayer.ShadewoodCharge += 0.1f;
+                        if (Main.rand.NextBool(3))
+                        {
+                            int d = Dust.NewDust(npc.position, npc.width, npc.height, DustID.Blood);
+                            Main.dust[d].velocity = Main.dust[d].position.DirectionTo(player.Center) * 4f;
+                        }
                     }
                         
                 }
             }
+            if (!found)
+                modPlayer.ShadewoodCharge -= 0.1f;
+            if (modPlayer.ShadewoodCharge >= MaxCharge)
+                modPlayer.ShadewoodCharge = MaxCharge;
+            if (player.whoAmI == Main.myPlayer)
+                CooldownBarManager.Activate("ShadewoodEnchantBuildup", FargoAssets.GetTexture2D("Content/Items/Accessories/Enchantments", "ShadewoodEnchant").Value, Color.DarkRed,
+                    () => (int)(Main.LocalPlayer.FargoSouls().ShadewoodCharge) / (float)MaxCharge, true, activeFunction: player.HasEffect<ShadewoodEffect>);
             if (!MoltenAuraProj.CombinedAura(player))
             {
                 int visualProj = ModContent.ProjectileType<ShadewoodAuraProj>();
@@ -94,35 +120,40 @@ namespace FargowiltasSouls.Content.Items.Accessories.Enchantments
                 }
             }
 
-            if (modPlayer.ShadewoodCD > 0)
-            {
-                modPlayer.ShadewoodCD--;
-            }
+        }
+        public static float GetScaledDamage(Player player, float sourceDamage)
+        {
+            bool force = player.ForceEffect<ShadewoodEffect>();
+            float softcapMult = force ? 2.5f : 1f;
+
+            if (sourceDamage > 50f * softcapMult)
+                sourceDamage = ((100f * softcapMult) + sourceDamage) / 3f;
+
+            sourceDamage += player.ForceEffect<ShadewoodEffect>() ? 45 : 15;
+            return sourceDamage;
         }
         public override void OnHitNPCEither(Player player, NPC target, NPC.HitInfo hitInfo, DamageClass damageClass, int baseDamage, Projectile projectile, Item item)
         {
-            ShadewoodProc(player, target, projectile);
+            ShadewoodProc(player, target, projectile, baseDamage);
         }
-        public static int BaseDamage(Player player)
-        {
-            int dmg = 12;
-            if (player.FargoSouls().ForceEffect<ShadewoodEnchant>())
-                dmg *= 3;
-            if (player.HasEffect<TimberEffect>())
-                dmg = (int)(dmg * 2.4f);
-            return (int)(dmg * player.ActualClassDamage(DamageClass.Melee));
-        }
-        public static void ShadewoodProc(Player player, NPC target, Projectile projectile)
+        public static void ShadewoodProc(Player player, NPC target, Projectile projectile, int baseDamage)
         {
             FargoSoulsPlayer modPlayer = player.FargoSouls();
             bool forceEffect = modPlayer.ForceEffect<ShadewoodEnchant>();
-
-            if (target.HasBuff(ModContent.BuffType<SuperBleedBuff>()) && modPlayer.ShadewoodCD == 0 && (projectile == null || projectile.type != ModContent.ProjectileType<SuperBlood>()) && player.whoAmI == Main.myPlayer)
+            if (modPlayer.ShadewoodCharge >= MaxCharge && (projectile == null || projectile.type != ModContent.ProjectileType<SuperBlood>()) && player.whoAmI == Main.myPlayer)
             {
-                modPlayer.ShadewoodCD = 40;
-                for (int i = 0; i < 2; i++)
+                modPlayer.ShadewoodCharge = 0;
+
+                float explosionDamage = GetScaledDamage(player, baseDamage);
+                float dirX = player.HorizontalDirectionTo(target.Center);
+                float max = 9;
+                for (int i = 0; i < max; i++)
                 {
-                    Projectile.NewProjectile(player.GetSource_Misc(""), target.Center.X, target.Center.Y - 20, 0f + Main.rand.NextFloat(-5, 5), Main.rand.NextFloat(-5, 5), ModContent.ProjectileType<SuperBlood>(), BaseDamage(player), 0f, Main.myPlayer);
+                    Vector2 vel = player.DirectionTo(target.Center).RotatedBy(MathHelper.PiOver2 * -0.15f * dirX + MathHelper.PiOver2 * 0.6f * (i - max / 2) / max).RotatedByRandom(MathHelper.PiOver2 * 0.05f);
+                    vel *= Main.rand.NextFloat(4, 6);
+                    vel *= 2;
+                    Vector2 center = Main.rand.NextVector2FromRectangle(target.Hitbox);
+                    Projectile.NewProjectile(player.GetSource_Misc(""), center, vel, ModContent.ProjectileType<SuperBlood>(), (int)explosionDamage, 0f, Main.myPlayer);
                 }
 
                 if (forceEffect)
