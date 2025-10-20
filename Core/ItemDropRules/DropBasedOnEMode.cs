725d952b54180ab9044020f0e1fde0bfc57ec252
Habble53 <87115317+Habble53@users.noreply.github.com>
2025-10-20 15:24:34 +0300

    EMode boss weapon drops:
    Now guaranteed to drop from any energized version.
    (TODO)Test and tweak weaker (if not at least the weakest) energized versions to be doable post boss

--- Core/ItemDropRules/DropBasedOnEMode.cs
+++ Core/ItemDropRules/DropBasedOnEMode.cs
@@ -1,4 +1,5 @@
-﻿using FargowiltasSouls.Core.ItemDropRules.Conditions;
+﻿using Fargowiltas.Content.NPCs;
+using FargowiltasSouls.Core.ItemDropRules.Conditions;
 using FargowiltasSouls.Core.Systems;
 using System.Collections.Generic;
 using Terraria.GameContent.ItemDropRules;
@@ -9,26 +10,38 @@ namespace FargowiltasSouls.Core.ItemDropRules
     {
         protected readonly IItemDropRule RuleForEMode;
         protected readonly IItemDropRule RuleForDefault;
+        protected readonly IItemDropRule RuleForSwarm;
 
         public List<IItemDropRuleChainAttempt> ChainedRules { get; private set; }
 
-        public DropBasedOnEMode(IItemDropRule ruleForEMode, IItemDropRule ruleForDefault)
+        public DropBasedOnEMode(IItemDropRule ruleForEMode, IItemDropRule ruleForDefault, IItemDropRule ruleForSwarm = null)
         {
             RuleForEMode = ruleForEMode;
             RuleForDefault = ruleForDefault;
+            RuleForSwarm = ruleForSwarm;
             ChainedRules = [];
         }
 
-        public bool CanDrop(DropAttemptInfo info) => WorldSavingSystem.EternityMode ? RuleForEMode.CanDrop(info) : RuleForDefault.CanDrop(info);
+        public bool CanDrop(DropAttemptInfo info)
+        {
+            bool swarm = info.npc.TryGetGlobalNPC<EnergizedGlobalNPC>(out var energizedNPC) && energizedNPC.SwarmActive(info.npc);
+            return swarm ? RuleForSwarm.CanDrop(info) : (WorldSavingSystem.EternityMode ? RuleForEMode.CanDrop(info) : RuleForDefault.CanDrop(info));
+        }
 
         public void ReportDroprates(List<DropRateInfo> drops, DropRateInfoChainFeed ratesInfo)
         {
             DropRateInfoChainFeed ratesInfo1 = ratesInfo.With(1f);
             ratesInfo1.AddCondition(new EModeDropCondition());
             RuleForEMode.ReportDroprates(drops, ratesInfo1);
+
             DropRateInfoChainFeed ratesInfo2 = ratesInfo.With(1f);
             ratesInfo2.AddCondition(new NotEModeDropCondition());
             RuleForDefault.ReportDroprates(drops, ratesInfo2);
+
+            DropRateInfoChainFeed ratesInfo3 = ratesInfo.With(1f);
+            ratesInfo3.AddCondition(new SwarmDropCondition());
+            RuleForDefault.ReportDroprates(drops, ratesInfo3);
+
             Chains.ReportDroprates(ChainedRules, 1f, drops, ratesInfo);
         }
 
@@ -37,7 +50,10 @@ namespace FargowiltasSouls.Core.ItemDropRules
             State = ItemDropAttemptResultState.DidNotRunCode
         };
 
-        public ItemDropAttemptResult TryDroppingItem(DropAttemptInfo info, ItemDropRuleResolveAction resolveAction) => WorldSavingSystem.EternityMode ? resolveAction(RuleForEMode, info) :
-            resolveAction(RuleForDefault, info);
+        public ItemDropAttemptResult TryDroppingItem(DropAttemptInfo info, ItemDropRuleResolveAction resolveAction)
+        {
+            bool swarm = info.npc.TryGetGlobalNPC<EnergizedGlobalNPC>(out var energizedNPC) && energizedNPC.SwarmActive(info.npc);
+            return swarm ? resolveAction(RuleForSwarm, info) : (WorldSavingSystem.EternityMode ? resolveAction(RuleForEMode, info) : resolveAction(RuleForDefault, info));
+        }
     }
 }
