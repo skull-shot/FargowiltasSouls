9d67fff2c27379f2afad4e74332ca900e27146a2
Midnight295 <143055111+Midnight295@users.noreply.github.com>
2025-10-26 14:51:42 -0700

    Resprited Trawler Soul. Resprited Mutant Eye Sphere projectiles. Resprited various Abominationn scythe projectiles.

--- Content/Bosses/AbomBoss/AbomScytheFlaming.cs
+++ Content/Bosses/AbomBoss/AbomScytheFlaming.cs
@@ -16,7 +16,6 @@ namespace FargowiltasSouls.Content.Bosses.AbomBoss
 
         public override void SetStaticDefaults()
         {
-            // DisplayName.SetDefault("Abominationn Scythe");
             ProjectileID.Sets.TrailCacheLength[Projectile.type] = 6;
             ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
         }
